046725e943dc3f2206424c03522a808b45d8270e
Philipp Diener <111326482+KaufmWhiskas@users.noreply.github.com>
2026-03-24 13:04:10 +0000

    Add basic styling file, with colors and basic css rules

---
+++ src/constants/theme.js
@@ -0,0 +1,18 @@
+export const COLORS = {
+  // Some default colors as placeholders, will change later
+  primary: '#2e64e5', // Main Color (for prominent features)
+  background: '#f8f9fa', //Default Background Color
+  surface: '#ffffff', //For Cards, Sheets, Inputs
+  text: '#1c1e21', //For most text
+  textLight: '#8a8d91', //For tex on dark backgrounds
+  success: '#28a745',
+  warning: '#ffc107',
+  danger: '#dc3545',
+};
+
+export const SIZES = {
+  base: 8,
+  padding: 16,
+  radius: 12,
+  largeRadius: 24,
+};
