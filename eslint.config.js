926b86f99e89708b15ee5b0eb806e0245c11e68c
KaufmWhiskas <philippdiener4@gmail.com>
2026-03-25 14:06:06 +0100

    update config to more modern standard

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,25 @@
-import expo from 'eslint-config-expo';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
 
 export default [
-  ...expo,
+  // This translates the old "expo" config into the new flat format
+  ...compat.extends('eslint-config-expo'),
+
+  // Prettier must be an object, not spread, usually placed at the end
   prettier,
+
   {
-    // Tell ESLint not to worry about Jest commands in test files
     files: ['**/*.test.js', '__tests__/**/*.js'],
     languageOptions: {
       globals: {
